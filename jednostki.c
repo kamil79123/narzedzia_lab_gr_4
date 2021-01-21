491d0ba0abf876b62d18c4282e1890b7a210c0f9
Radek Barczynski <radbar005@utp.edu.pl>
2021-01-21 11:26:22 -0800

    Part Radka

---
+++ jednostki.c
@@ -0,0 +1,35 @@
+void jednostki()
+{
+    int wyb;
+    float dlugosc;
+    float wynik;
+    printf("\nWybierz, co chcesz przeliczyc:");
+    printf("\n1. Milimetry na centymetry.");
+    printf("\n2. Milimetry na metry.");
+    scanf("%d", &wyb);
+    switch (wyb)
+    {
+    case 1:
+    {
+        printf("Wprowadz dlugosc w mm");
+        scanf("%f", dlugosc);
+        wynik = dlugosc * 10;
+        printf("%f milimetrow to %f centymetrow.", dlugosc, wynik);
+    }
+    break;
+    case 2:
+    {
+        printf("Wprowadz dlugosc w mm");
+        scanf("%f", dlugosc);
+        wynik = dlugosc * 100;
+        printf("%f milimetrow to %f metrow.", dlugosc, wynik);
+    }
+    break;
+    default:
+    {
+        printf("Wybrano zla opcje!");
+        return 0;
+    }
+    }
+    return;
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "jednostki.h"
+#include "plik.h"
+#include "XD.h"
+
+
+int main()
+{
+printf("Radek tu byl\n");
+hello();
+jednostki();
+summation();
+}
+
