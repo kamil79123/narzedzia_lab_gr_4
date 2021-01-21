4c5a81fd7b839dd86c975910db51b9ce1c1112f3
kamil_np <kamil79123@gmail.com>
2021-01-21 19:06:19 +0100

    Dodano opcję przeliczania milimetrów na metry

--- jednostki.c
+++ jednostki.c
@@ -5,10 +5,31 @@ void jednostki()
     float wynik;
     printf("\nWybierz, co chcesz przeliczyc:");
     printf("\n1. Milimetry na centymetry.");
+    printf("\n2. Milimetry na metry.");
     scanf("%d", &wyb);
-    printf("Wprowadz dlugosc w mm");
-    scanf("%f", dlugosc);
-    wynik = dlugosc * 10;
-    printf("%f milimetrow to %f centymetrow.", dlugosc, wynik);
+    switch (wyb)
+    {
+    case 1:
+    {
+        printf("Wprowadz dlugosc w mm");
+        scanf("%f", dlugosc);
+        wynik = dlugosc * 10;
+        printf("%f milimetrow to %f centymetrow.", dlugosc, wynik);
+    }
+    break;
+    case 2:
+    {
+        printf("Wprowadz dlugosc w mm");
+        scanf("%f", dlugosc);
+        wynik = dlugosc * 100;
+        printf("%f milimetrow to %f metrow.", dlugosc, wynik);
+    }
+    break;
+    default:
+    {
+        printf("Wybrano zla opcje!");
+        return 0;
+    }
+    }
     return;
 }
