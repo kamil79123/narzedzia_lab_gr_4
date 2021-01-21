8e3762c271677f818dd92553accb62caa4a6e255
yourname <your-email@xxx.com>
2021-01-21 20:28:37 +0100

    5

--- licznik.c
+++ licznik.c
@@ -4,10 +4,10 @@ void licz()
 	printf("Podaj liczbe: ");
 	scanf("%i", &a);
 	printf("\r\nLiczba: %i\r\n", a);
-	printf("Podaj liczbe 1-9");
+	printf("\r\nPodaj liczbe 1-9\r\n");
 	scanf("%i", &b);
-	if(b < 1) printf("Zla liczba");
-	else if(b > 9) printf("Zla liczba");
-	else printf("Liczba jest rowna: %i", b);
+	if(b < 1) printf("\r\nZla liczba");
+	else if(b > 9) printf("\r\nZla liczba");
+	else printf("\r\nLiczba jest rowna: %i", b);
 	
 }
